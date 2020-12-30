bc4271a9ed49396a37f792f2473200a8b625d975
Corbin Crutchley <corbin.crutchley@websystem3.com>
2020-12-30 12:28:02 -0800

    refactor: initial work to support arbitrary file storage

--- ios/RNDirectoryPicker.swift
+++ ios/RNDirectoryPicker.swift
@@ -6,8 +6,27 @@ class DirectoryPicker: NSObject, UIDocumentPickerDelegate {
   var resolve: RCTPromiseResolveBlock? = nil;
   var reject: RCTPromiseRejectBlock? = nil;
   func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
-    let paths = urls.map({ $0.path })
-    resolve!(paths)
+    let url = urls[0]
+    
+    // Start accessing a security-scoped resource.
+    guard url.startAccessingSecurityScopedResource() else {
+      // Handle the failure here.
+      return
+    }
+    
+    // Make sure you release the security-scoped resource when you are done.
+    defer { url.stopAccessingSecurityScopedResource() }
+    
+    do {
+      let bookmarkData = try url.bookmarkData(options: .minimalBookmark, includingResourceValuesForKeys: nil, relativeTo: nil)
+      
+      let filename = getDocumentsDirectory().appendingPathComponent("test")
+      
+      try bookmarkData.write(to: filename)
+      resolve!("")
+    } catch {
+      reject!(error.localizedDescription, error.localizedDescription, error)
+    }
   }
   
   func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
@@ -41,34 +60,53 @@ class DirectoryPicker: NSObject, UIDocumentPickerDelegate {
   }
   
   @objc
+  func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+  }
+  
+  @objc
   static func requiresMainQueueSetup() -> Bool {
     return true
   }
   
   @objc(createFolder:folderName:)
   public func createFolder(_ path: String, folderName: String) {
-    let url = URL(fileURLWithPath: path);
-//    let needTo = url.startAccessingSecurityScopedResource()
-    guard url.startAccessingSecurityScopedResource() else {
-        // Handle the failure here.
-        print("THERE WAS A FAILURE HERE")
-        return
+    var url: URL? = nil
+    
+    do {
+      let filename = getDocumentsDirectory().appendingPathComponent("test")
+      let bookmarkData = try Data(contentsOf: filename)
+      var isStale = false
+      url = try URL(resolvingBookmarkData: bookmarkData, bookmarkDataIsStale: &isStale)
+    } catch {
+      print(error.localizedDescription)
+      return
     }
-
-    defer { url.stopAccessingSecurityScopedResource() }
-
+    
+    //    let url = URL(fileURLWithPath: path);
+    //    let needTo = url.startAccessingSecurityScopedResource()
+    guard url!.startAccessingSecurityScopedResource() else {
+      // Handle the failure here.
+      print("THERE WAS A FAILURE HERE")
+      return
+    }
+    
+    defer { url!.stopAccessingSecurityScopedResource() }
+    
     let fileManager = FileManager.default
-    let filePath =  url.appendingPathComponent("\(folderName)")
+    let filePath =  url!.appendingPathComponent("\(folderName)")
     do {
       try fileManager.createDirectory(atPath: filePath.path, withIntermediateDirectories: true, attributes: nil)
     } catch {
       print(error.localizedDescription)
     }
     
-//
-//    if needTo {
-//      url.stopAccessingSecurityScopedResource()
-//      print("I am done")
-//    }
+    //
+    //    if needTo {
+    //      url.stopAccessingSecurityScopedResource()
+    //      print("I am done")
+    //    }
   }
 }
+
